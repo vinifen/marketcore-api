d0ef40c204a46459b5b67d5cc4ba5c6e3ff5507c
vinifen <164583834+vinifen@users.noreply.github.com>
2025-07-13 21:11:02 -0300

    🐛 fix docker permission

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache /var/www/.env
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
-chmod 664 /var/www/.env
+
 
 exec "$@"
