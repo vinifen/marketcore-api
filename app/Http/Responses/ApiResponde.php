0c640b113319f87cb567ed2f15f1b3606feeb3c7
vinifen <164583834+vinifen@users.noreply.github.com>
2025-07-16 10:56:48 -0300

    ⚙️  start api setup

---
+++ app/Http/Responses/ApiResponde.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Http\JsonResponse;
+
+trait ApiResponse
+{
+    protected function success($data = null, $message = null, $status = 200): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $status);
+    }
+
+    protected function error($message = 'Unexpected error', $status = 400, $errors = null): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'errors' => $errors,
+        ], $status);
+    }
+}
