a9171c10b778b66710716186c70b7f416bf19630
vinifen <164583834+vinifen@users.noreply.github.com>
2025-07-31 19:04:47 -0300

    ♻️ organize files

--- app/Http/Resources/DiscountResource.php
+++ app/Http/Resources/DiscountResource.php
@@ -8,8 +8,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class DiscountResource extends JsonResource
 {
     /**
-     * Transform the resource into an array.
-     *
      * @return array<string, mixed>
      */
     public function toArray(Request $request): array
--- tests/Feature/Catalog/Discount/DiscountControllerTest.php
+++ tests/Feature/Catalog/Discount/DiscountControllerTest.php
@@ -3,14 +3,11 @@
 namespace Tests\Feature\Catalog\Discount;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DiscountControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
+    use RefreshDatabase;
     public function test_example(): void
     {
         $response = $this->get('/');
