fc041b0c58b32eb521878a30337b01ed7af08ef9
vinifen <164583834+vinifen@users.noreply.github.com>
2025-07-31 19:13:12 -0300

    ⚙️ add database

--- database/factories/DiscountFactory.php
+++ database/factories/DiscountFactory.php
@@ -3,19 +3,18 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Discount>
- */
 class DiscountFactory extends Factory
 {
-    /**
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'product_id' => Product::factory(),
+            'description' => $this->faker->sentence(3),
+            'startDate' => $this->faker->dateTimeBetween('-1 month', 'now')->format('Y-m-d'),
+            'endDate' => $this->faker->dateTimeBetween('now', '+1 month')->format('Y-m-d'),
+            'discountPercentage' => $this->faker->randomFloat(2, 1, 50),
         ];
     }
 }
--- database/migrations/2025_07_31_215241_create_discounts_table.php
+++ database/migrations/2025_07_31_215241_create_discounts_table.php
@@ -10,7 +10,14 @@ return new class extends Migration
     {
         Schema::create('discounts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->string('description');
+            $table->date('startDate');
+            $table->date('endDate');
+            $table->decimal('discountPercentage', 5, 2);
             $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
