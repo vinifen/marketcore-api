65a58ce60bdef83c96fda52e34b3426654539dea
vinifen <164583834+vinifen@users.noreply.github.com>
2025-08-01 11:38:03 -0300

    ⚙️ create model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -5,8 +5,37 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property int $user_id
+ *
+ * @property-read \App\Models\User $user
+ */
 class Cart extends Model
 {
     /** @use HasFactory<\Database\Factories\CartFactory> */
     use HasFactory;
+
+    /**
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+    ];
+
+    /**
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'user_id' => 'integer',
+    ];
+
+    /**
+     * Get the user that owns the cart.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
