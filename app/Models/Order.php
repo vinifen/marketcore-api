cf8802b97bbacf5c378cf09ecc11707124f50459
vinifen <164583834+vinifen@users.noreply.github.com>
2025-08-03 11:31:11 -0300

    ✨ create model and remove oid

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,9 +4,63 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Enums\OrderStatus;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property int|null $user_id
+ * @property int|null $address_id
+ * @property int|null $coupon_id
+ * @property string $order_date
+ * @property float $total_amount
+ * @property OrderStatus $status
+ */
 class Order extends Model
 {
     /** @use HasFactory<\Database\Factories\OrderFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'address_id',
+        'coupon_id',
+        'order_date',
+        'total_amount',
+        'status',
+    ];
+
+    protected $casts = [
+        'id' => 'integer',
+        'user_id' => 'integer',
+        'address_id' => 'integer',
+        'coupon_id' => 'integer',
+        'order_date' => 'datetime',
+        'total_amount' => 'float',
+        'status' => OrderStatus::class,
+    ];
+
+    /**
+     * @return BelongsTo<User, Order>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return BelongsTo<Address, Order>
+     */
+    public function address(): BelongsTo
+    {
+        return $this->belongsTo(Address::class);
+    }
+
+    /**
+     * @return BelongsTo<Coupon, Order>
+     */
+    public function coupon(): BelongsTo
+    {
+        return $this->belongsTo(Coupon::class);
+    }
 }
--- database/migrations/2025_08_03_135939_create_orders_table.php
+++ database/migrations/2025_08_03_135939_create_orders_table.php
@@ -10,6 +10,35 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('address_id')->nullable();
+            $table->dateTime('order_date');
+            $table->unsignedBigInteger('coupon_id')->nullable();
+            $table->decimal('total_amount', 10, 2);
+
+            $table->enum('status', [
+                'PENDING',
+                'PROCESSING',
+                'SHIPPED',
+                'COMPLETED',
+                'CANCELED'
+            ])->default('PENDING');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+
+            $table->foreign('address_id')
+                ->references('id')
+                ->on('addresses')
+                ->onDelete('set null');
+
+            $table->foreign('coupon_id')
+                ->references('id')
+                ->on('coupons')
+                ->onDelete('set null');
+
             $table->timestamps();
         });
     }
